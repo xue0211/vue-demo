3effcd4e9a62313934943d73d23cdd92c2f882fa
xue0211 <zhengtai1123.qq.com>
2024-11-07 18:41:21 +0800

    学习vue

---
+++ vue-demo-2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    {{ user }}
+    <hr>
+    登录
+    <form @click.prevent="onsubmit"> 
+      <label>
+        <span>用户名</span>
+        <input type="text" :value="user.username" 
+        @input="user.username = $event.target.value" />
+      </label>
+      <label>
+        <span>密码</span>
+        <input type="password" v-model="user.password" />
+      </label>
+      <button type="submit">登录</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    onsubmit() {
+      console.log(this.user)
+    }
+  }
+}
+</script>
